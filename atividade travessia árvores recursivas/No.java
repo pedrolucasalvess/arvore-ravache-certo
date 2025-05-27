27badd9be698ab8563608a7b81401ae442ff3e03
Pedro Lucas <pedrolucas.marlier@gmail.com>
2025-05-27 09:01:04 -0300

    criação travessia em nível

--- atividade travessia árvores recursivas/No.java
+++ atividade travessia árvores recursivas/No.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 class No {
     String valor;
     No esquerda, direita;
@@ -38,5 +41,18 @@ class No {
         }
     }
 
+    public void emNivel(No raiz){
+        if (raiz == null) return;
 
+        Queue<No> fila = new LinkedList<>();
+        fila.add(raiz);
+
+        while (!fila.isEmpty()){
+            No atual = fila.poll();
+            System.out.print(atual.valor + " ");
+
+            if (atual.esquerda != null) fila.add(atual.esquerda);
+            if (atual.direita != null) fila.add(atual.direita);
+        }
+    }
 }
