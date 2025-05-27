46a2ad0d7ed2e9cd7e48c7cfcf5462de6463c7ea
Pedro Lucas <pedrolucas.marlier@gmail.com>
2025-05-27 09:19:35 -0300

    criação pós ordem iterativo

--- atividade travessia árvores sem recursividade/No.java
+++ atividade travessia árvores sem recursividade/No.java
@@ -60,6 +60,26 @@ class No {
             atual = atual.direita;
         }
     }
+    
+    public void posOrdemIterativa(No raiz) {
+    if (raiz == null) return;
 
+        Stack<No> pilha1 = new Stack<>();
+        Stack<No> pilha2 = new Stack<>();
+
+        pilha1.push(raiz);
+
+        while (!pilha1.isEmpty()) {
+            No atual = pilha1.pop();
+            pilha2.push(atual);
+
+            if (atual.esquerda != null) pilha1.push(atual.esquerda);
+            if (atual.direita != null) pilha1.push(atual.direita);
+        }
+
+        while (!pilha2.isEmpty()) {
+            System.out.print(pilha2.pop().valor + " ");
+        }
+    }
 
 }
