15d59530520e53a5bb9562f5ea0fd4784c15fa7f
Pedro Lucas <pedrolucas.marlier@gmail.com>
2025-05-27 09:20:45 -0300

    reutilização da travessia em nível já iterativa

--- atividade travessia árvores sem recursividade/No.java
+++ atividade travessia árvores sem recursividade/No.java
@@ -82,4 +82,19 @@ class No {
         }
     }
 
+    public void emNivel(No raiz){
+        if (raiz == null) return;
+
+        Queue<No> fila = new LinkedList<>();
+        fila.add(raiz);
+
+        while (!fila.isEmpty()){
+            No atual = fila.poll();
+            System.out.print(atual.valor + " ");
+
+            if (atual.esquerda != null) fila.add(atual.esquerda);
+            if (atual.direita != null) fila.add(atual.direita);
+        }
+    }
+
 }
