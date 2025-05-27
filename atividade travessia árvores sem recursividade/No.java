0e1a5a00bc6b89ac6fa0b7b4c2e35813c6b60d83
Pedro Lucas <pedrolucas.marlier@gmail.com>
2025-05-27 20:23:16 -0300

    criação travessia contar nós folha

--- atividade travessia árvores sem recursividade/No.java
+++ atividade travessia árvores sem recursividade/No.java
@@ -97,4 +97,23 @@ class No {
         }
     }
 
+     public int contarNosFolha(No raiz) {
+        if (raiz == null) return 0;
+
+        Stack<No> pilha = new Stack<>();
+        pilha.push(raiz);
+        int contador = 0;
+
+        while (!pilha.isEmpty()) {
+            No atual = pilha.pop();
+
+            if (atual.esquerda == null && atual.direita == null) {
+                contador++;
+            }
+
+        if (atual.direita != null) pilha.push(atual.direita);
+        if (atual.esquerda != null) pilha.push(atual.esquerda);
+    }
+    return contador;
+    }
 }
