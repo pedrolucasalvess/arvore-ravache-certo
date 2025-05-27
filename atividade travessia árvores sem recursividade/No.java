71a5e232094c1b58a70be2ba79f3f29e31a67c2a
Pedro Lucas <pedrolucas.marlier@gmail.com>
2025-05-27 09:18:18 -0300

    criação pré ordem iterativo

--- atividade travessia árvores sem recursividade/No.java
+++ atividade travessia árvores sem recursividade/No.java
@@ -1,5 +1,6 @@
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 class No {
     String valor;
@@ -28,4 +29,20 @@ class No {
 
     return contador;
     }
+
+    public void preOrdemIterativa(No raiz) {
+    if (raiz == null) return;
+
+        Stack<No> pilha = new Stack<>();
+        pilha.push(raiz);
+
+        while (!pilha.isEmpty()) {
+            No atual = pilha.pop();
+            System.out.print(atual.valor + " ");
+
+            if (atual.direita != null) pilha.push(atual.direita);
+            if (atual.esquerda != null) pilha.push(atual.esquerda);
+        }
+    }
+
 }
